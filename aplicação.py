c9d5e54bd84e5141833da66ed702f60fd3e4fb17
Alekao18 <umzehonorio@gmail.com>
2024-12-28 11:24:18 -0300

    Utilizando e compreendendo o funcionamento do mode lo de IA

--- aplicação.py
+++ aplicação.py
@@ -7,4 +7,10 @@ os.environ ['GROQ_API_KEY'] = api_key # variável de ambiente
 from langchain_groq import ChatGroq #biblioteca da Groq
 #Langchain puxa outras bibliotecas menores
 
-chat = ChatGroq(model = 'llama-3.3-70b-versatile') #inicializando o chatbot com o modelo Ilhama3(fornecido pela Groq)
+chat = ChatGroq(model = 'llama-3.3-70b-versatile') #inicializando o chatbot com o modelo llama3(fornecido pela Groq)
+#llama3 é um modelo de linguagem grande e gratuito da META
+
+resposta = chat.invoke('O que é a vida?')
+print(resposta.content)#mostra a resposta do chatbot invocado
+#.content, mostra apenas o conteúdo do que a gente pediu(reposta)
+#ll3(llama3) um modelo de IA programada com diversos parâmetros , como a capacidade de aprender, raciocinar e etc. Pronta para acessarmos e utilizarmos!
